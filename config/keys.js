f75ced96a163bfb1f7572a24b86e05f8756fc445
gitchili <jameslphillips@gmail.com>
2018-06-16 21:24:34 -0600

    fix
    error fixing

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
 // stripe API keys 
-if(process.env.NODE_ENV === 'test'){
+if(process.env.NODE_ENV === 'production'){
     module.exports = require('./keys_prod');
   } else {
     module.exports = require('./keys_dev');
